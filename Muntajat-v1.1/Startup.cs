25061c8efa2e378b6bbc2cf605d193d33afc9265
Shaikhah Maasher <sh.maasher@gmail.com>
2017-11-07 10:46:45 +0300

    Add project files.

---
+++ Muntajat-v1.1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Muntajat_v1._1.Startup))]
+namespace Muntajat_v1._1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
